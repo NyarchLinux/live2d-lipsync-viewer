d4ebbaef0312a6a3ea88a3ff4d88c2ca16e8f63d
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-12-27 10:44:12 +0100

    Update offline packaging

--- package.sh
+++ package.sh
@@ -8,8 +8,8 @@ rm -rf .git
 rm -rf audio
 wget https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js
 wget https://cdn.jsdelivr.net/gh/dylanNew/live2d/webgl/Live2D/lib/live2d.min.js
-wget https://cdn.jsdelivr.net/npm/pixi.js@6.5.2/dist/browser/pixi.min.js
-wget https://cdn.jsdelivr.net/gh/RaSan147/pixi-live2d-display@1.0.6/dist/cubism4.min.js
+wget https://cdn.jsdelivr.net/npm/pixi.js@7.x/dist/pixi.min.js
+wget https://cdn.jsdelivr.net/gh/RaSan147/pixi-live2d-display@v0.5.0-ls-7/dist/index.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css
 cd ..
 tar -cJf pack.tar.xz build
